c208ac091199f6fe0f37dd297a39d3e51db766de
mechwtf-cloud <mech.wtf@gmail.com>
2026-03-19 16:13:50 +0100

    Refactor square generation and remove prompts

--- scripts.js
+++ scripts.js
@@ -1,17 +1,50 @@
-const btn = document.getElementById("startBtn");
-const output = document.getElementById("output");
+function generateSquares() {
+    const count = document.getElementById("count").value;
+    const output = document.getElementById("output");
 
-btn.onclick = startProgram;
+    output.innerHTML = "";
 
-function startProgram(){
+    for (let i = 1; i <= count; i++) {
+        const div = document.createElement("div");
+        div.classList.add("box");
 
-    let name = prompt("Name?");
-    let age = prompt("Age?");
+        // random farba
+        div.style.backgroundColor = getRandomColor();
+        div.textContent = i;
 
-    if(age >= 18){
-        output.textContent = name + " is an adult";
-    }else{
-        output.textContent = name + " is under 18";
+        output.appendChild(div);
+    }
+}
+
+function generateNumbers() {
+    const count = document.getElementById("count").value;
+    const output = document.getElementById("output");
+
+    output.innerHTML = "";
+
+    for (let i = 1; i <= count; i++) {
+        const div = document.createElement("div");
+        div.classList.add("box");
+
+        div.style.backgroundColor = "#111827";
+        div.style.color = "#22c55e";
+
+        // rastúca veľkosť textu
+        div.style.fontSize = (10 + i * 2) + "px";
+
+        div.textContent = i;
+
+        output.appendChild(div);
+    }
+}
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
 
+    return color;
 }
